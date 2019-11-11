687b0e688cb5c1913375d9c76f3735139216bf0c
ziyaliuye <171242039@qq.com>
2019-11-11 23:28:59 +0800

    完善代码...

--- src/main/java/cn/cnm/test/SpringMapperDemo.java
+++ src/main/java/cn/cnm/test/SpringMapperDemo.java
@@ -1,8 +1,12 @@
 package cn.cnm.test;
 
 import cn.cnm.mapper.AirportMapper;
+import cn.cnm.pojo.Flower;
+import cn.cnm.service.CostomService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 /**
  * @author lele
@@ -15,6 +19,9 @@ public class SpringMapperDemo {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-context.xml");
         // 直接获取Mapper的接口即可
-        AirportMapper airportMapper = applicationContext.getBean("airportMapper", AirportMapper.class);
+        CostomService costomService = applicationContext.getBean("costomService", CostomService.class);
+
+        List<Flower> list = costomService.getAll();
+        list.forEach(System.out::println);
     }
 }
